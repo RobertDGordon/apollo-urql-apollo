a66402b0efd9313e113962f7e9e45ac342a91620
Robert Gordon <Komeuppance@gmail.com>
2020-02-25 08:25:52 -0800

    Apollo server online

---
+++ server/datasources/user.js
@@ -0,0 +1,61 @@
+const { DataSource } = require("apollo-datasource")
+
+class UserAPI extends DataSource {
+    constructor({ store }) {
+        super()
+        this.store = store
+    }
+
+    initialize(config) {
+        this.context = config.context
+    }
+
+    async getUsers() {
+        const users = this.store.users
+        return users
+    }
+
+    async getUser({email: emailArg}) {
+        let index = 1;
+        const email = this.context && this.context.user ?
+            this.context.user.email : emailArg;
+        
+        const theUser = this.store.users.map(user => {
+            if (email === user.email) {
+                index = this.store.users.indexOf(user)
+                console.log('user index:', user.id, index)
+                return user
+            }
+        })
+
+        return theUser[index]
+    }
+    async saveRecord({ recordId }) {
+        const userId = this.context.user.id;
+        // console.log('userId', userId)
+        if(!userId) {
+            console.log('No user on context')
+        } else {
+            console.log('User context:', userId)
+        }
+
+        const usercheck = this.store.users.map(user => {
+            if(userId == user.id) {
+                user.records.push({ id: recordId })
+                return user
+            }
+        })
+
+        let users = [];
+
+        await usercheck.forEach(e => {
+            if(e) {
+                users.push(e)
+            }
+        })
+
+        return users[0].records.length > 4 ? users[0].records : "Record length error"
+    }
+}
+
+module.exports = UserAPI
---
+++ server/index.js
@@ -0,0 +1,58 @@
+const { ApolloServer } = require('apollo-server')
+const typeDefs = require('./schema.js');
+const resolvers = require('./resolvers');
+
+const { createStore } = require('./utils.js')
+
+const QuakeAPI = require('./datasources/quake.js');
+const UserAPI = require('./datasources/user.js');
+
+const store = createStore();
+
+const server = new ApolloServer({
+    context: async ({ req }) => {
+        // simple auth check on every request
+        const auth = req.headers && req.headers.authorization || '';
+        
+        let email = '';
+        let token = '';
+
+        const getToken = () => {
+            return auth.split(' ')[1]
+        }
+        if (auth.length && auth.split(' ')[1]){
+            token = getToken()
+        }
+
+        // find a user by their email
+        const usercheck = await store.users.map(user => {
+            if (email === user.email) {
+                // console.log('user: ',user)
+                return user
+            }
+        });
+
+        let users = []
+
+        await usercheck.forEach(e => {
+            if (e) {
+                // console.log('pushing:', e)
+                users.push(e)
+            }
+        })
+
+        const user = users && users[0] || null;
+
+        return { user };
+      },
+    typeDefs,
+    resolvers,
+    dataSources: () => ({
+        quakeAPI: new QuakeAPI(),
+        userAPI: new UserAPI({ store })
+    })
+});
+
+server.listen().then(({ url }) => {
+    console.log(`Server online at ${url}`);
+});
